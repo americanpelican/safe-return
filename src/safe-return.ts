b45856a2c39d652973853c9ef8b9eacc748d9f61
americanpelican <19810103+americanpelican@users.noreply.github.com>
2024-07-05 10:18:53 -0400

    Update safe-return.ts

--- src/safe-return.ts
+++ src/safe-return.ts
@@ -1,4 +1,10 @@
-// reference: https://betterprogramming.pub/typescript-with-go-rust-errors-no-try-catch-heresy-da0e43ce5f78
+/**
+ * @module
+ * This module contains functions to search the database.
+ *
+ * @remarks reference: https://betterprogramming.pub/typescript-with-go-rust-errors-no-try-catch-heresy-da0e43ce5f78
+ *
+ */
 
 // TODO: incorporate safe functions in article
 
@@ -8,24 +14,51 @@ interface SafeBase {
 }
 
 // --- export types ---
+/**
+ * Represents a safe data object with a success flag and data of type T.
+ * @template T - The type of the data.
+ */
 export interface SafeData<T> extends SafeBase {
     success: true
     data: T
   }
 
+/**
+ * Represents an error response in the SafeReturn module.
+ */
 export interface SafeError extends SafeBase {
     success: false;
     error: string;
   }
 
+/**
+ * Represents a safe value or an error.
+ * @typeparam T The type of the safe value.
+ */
 export type Safe<T> =
   | SafeData<T>
   | SafeError
 
 
+/**
+ * Represents a safe return value of type `T`.
+ *
+ * @template T - The type of the safe return value.
+ */
 export type SafeReturn<T> = Safe<T> // TODO: retire SafeReturn for shortness
 
 // --- local functions ---
+
+
+/**
+ * Executes a synchronous function and returns a Safe object containing the result.
+ * If an error occurs during execution, it is converted to a Safe object using the errorToSafe function.
+ *
+ * @template T - The type of the data returned by the function.
+ * @param {() => T} func - The function to execute.
+ * @param {string} [err] - An optional error message to include in the Safe object if an error occurs.
+ * @returns {Safe<T>} - A Safe object containing the result of the function execution.
+ */
 function safeSync<T>(func: () => T, err?: string): Safe<T> {
 	try {
 		const data = func()
@@ -55,6 +88,12 @@ export function safe<T> (promiseOrFunc: Promise<T> | (() => T), err?: string,): 
 	return safeSync(promiseOrFunc, err);
 }
 
+/**
+ * Converts an error object or error message to a SafeError object.
+ * @param e - The error object or error message.
+ * @param err - An optional error message to use if `e` is not an instance of Error.
+ * @returns A SafeError object representing the converted error.
+ */
 export function errorToSafe (e: unknown, err?: string): SafeError {
 	if (err !== undefined) return { success: false, error: err }
 
